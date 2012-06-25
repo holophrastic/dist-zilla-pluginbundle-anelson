972b1291afdff7e0a31922b4fdab5de421a12d6b
Andrew Nelson <anelson@cpan.org>
2012-06-24 23:18:50 -0500

    Initial commit.

---
+++ lib/Dist/Zilla/PluginBundle/ANELSON.pm
@@ -0,0 +1,113 @@
+package Dist::Zilla::PluginBundle::ANELSON;
+use Moose;
+
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+# ABSTRACT: Dist::Zilla plugins for anelson
+
+=head1 DESCRIPTION
+
+This is the plugin bundle that ANELSON uses.  It's equivalent to:
+
+    [@Basic]
+
+    [Git::NextVersion]
+    first_version   = 0.01
+    version_regexp  = ^(\d+\.\d+)$
+
+    [ReadmeMarkdownFromPod]
+
+    [PkgVersion]
+
+    [AutoPrereqs]
+
+    [NextRelease]
+    format          = %v %{MMM d yyyy}d
+
+    [SynopsisTests]
+
+    [PodSyntaxTests]
+
+    [MetaJSON]
+
+    [GithubMeta]
+    issues = 1
+
+    [CopyFilesFromBuild]
+    copy            = README.mkdn
+
+    [PruneFiles]
+    filenames       = dist.ini
+
+    [Git::Commit]
+
+    [Git::Tag]
+    tag_format      = %v
+
+    [PodWeaver]
+
+=cut
+
+sub configure {
+    my ($self) = @_;
+
+    $self->add_bundle('@Basic');
+
+    $self->add_plugins([
+        'Git::NextVersion' => {
+            first_version  => '0.01',
+            version_regexp => '^(\d+\.\d+)$'
+        }
+    ]);
+
+    $self->add_plugins(qw(
+        ReadmeMarkdownFromPod
+        PkgVersion
+        AutoPrereqs
+    ));
+
+    $self->add_plugins([
+        'NextRelease' => {
+            format => '%v %{MMM d yyyy}d'
+        }
+    ]);
+
+    $self->add_plugins(qw(
+        SynopsisTests
+        PodSyntaxTests
+        MetaJSON
+    ));
+
+    $self->add_plugins([
+        'GithubMeta' => {
+            issues => 1
+        }
+    ]);
+
+    $self->add_plugins([
+        'CopyFilesFromBuild' => {
+            copy => 'README.mkdn'
+        }
+    ]);
+    
+    $self->add_plugins([
+        'PruneFiles' => {
+            filenames => [ qw(dist.ini weaver.ini) ]
+        }
+    ]);
+
+    $self->add_plugins(qw(Git::Commit));
+
+    $self->add_plugins([
+        'Git::Tag' => {
+            tag_format => '%v'
+        }
+    ]);
+
+    $self->add_plugins(qw(PodWeaver));
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
